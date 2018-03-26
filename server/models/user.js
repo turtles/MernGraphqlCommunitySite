8b75d630e8b2a9f03f03d25537344391c5340fae
Leah Sandacz <sandas@outlook.com>
2018-03-26 10:16:18 -0700

    User model so far

---
+++ server/models/user.js
@@ -0,0 +1,90 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
+const bcrypt = require('bcryptjs');
+
+const secretSalt = 'z#fPx%v8i3'
+
+var UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minLength: 1,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email'
+    }
+  },
+  password: {
+    type: String,
+    require: true,
+    minLength: 6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }]
+});
+
+UserSchema.pre('save', function (next) {
+  var user = this;
+
+  if (user.isModified('password')) {
+    .genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = (hash);
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+})
+
+UserSchema.methods.toJSON = function() {
+  var user = this;
+  var userObject = user.toObject();
+  return _.pick(userObject, ['_id', 'email']);
+}
+
+UserSchema.methods.generateAuthToken = function () {
+  var user = this;
+  var access = 'auth';
+  var token = jwt.sign({_id: user._id.toHexString(), access}, secretSalt).toString();
+
+  user.tokens.push({access, token});
+
+  return user.save().then(()=>{
+    return token;
+  });
+}
+
+UserSchema.statics.findByToken = function (token) {
+  var User = this;
+  var decoded;
+
+  try {
+    decoded = jwt.verify(token, secretSalt);
+  } catch (e) {
+
+  }
+
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  });
+}
+
+var User = mongoose.model('User', UserSchema);
+
+module.exports = {User};
--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const _ = require('lodash')
 const {mongoose} = require('./db/mongoose')
+const {User} = require('./models/user')
+const {authenticate} = require('./middleware/authenticate');
 
 var app = express()
 app.use(function(req, res, next) {
@@ -21,6 +24,25 @@ app.get('/', (req,res) => {
 app.get('/hello', (req,res) => {
      res.send({message: "hello ' w '"});
 })
+
+// Register user route
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token)=> {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  })
+})
+
+// Private route
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
+});
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`)
