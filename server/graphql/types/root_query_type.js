4d78b8b9113c62937cf30cfa08f5dba88c7c11d1
Leah Sandacz <sandas@outlook.com>
2018-04-22 00:45:24 -0700

    Handle querying for articles serverside

--- server/graphql/types/root_query_type.js
+++ server/graphql/types/root_query_type.js
@@ -1,8 +1,15 @@
+const mongoose = require('mongoose');
 const graphql = require('graphql');
-const { GraphQLObjectType, GraphQLID } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLList
+} = graphql;
 
 const UserType = require('./user_type');
 const ArticleType = require('./article_type');
+
+const Article = mongoose.model('article');
 
 const RootQueryType = new GraphQLObjectType({
   name: 'RootQueryType',
@@ -14,9 +21,14 @@ const RootQueryType = new GraphQLObjectType({
       }
     },
     article: {
-      type: ArticleType,
+      type: new GraphQLList(ArticleType),
       resolve(parentValue, args, req) {
-        return null;
+        return new Promise((resolve, reject) => {
+          Article.find((err, articles) => {
+            if (err) reject(err);
+            resolve(articles);
+          })
+        })
       }
     }
   }
