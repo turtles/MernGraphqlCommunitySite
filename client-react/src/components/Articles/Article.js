af982888a43b340599658420ca321824d41781e6
Leah Sandacz <sandas@outlook.com>
2018-04-25 15:49:03 -0700

    Article page should query for an article based on the route

--- client-react/src/components/Articles/Article.js
+++ client-react/src/components/Articles/Article.js
@@ -1,16 +1,39 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
+import { withRouter } from 'react-router-dom';
+import query from '../../graphql/queries/FindArticle';
 
-import query from '../../graphql/queries/Article';
+import NotificationError from '../Errors/NotificationError';
 
 class Article extends Component {
   render() {
+    const { loading, error } = this.props.data;
+    if (loading) return <div/>;
+    else if (error) return <NotificationError error={error}/>;
+
+    const { title, body, views } = this.props.data.article;
     return (
       <div>
-        <h3>{this.props.articleId}</h3>
+        <h3>{title}</h3>
+        <p>{body}</p>
+        <p>{views} views</p>
       </div>
     );
   }
 }
 
-export default graphql(query)(Article);
+export default graphql(query,
+  {
+    options:
+      (props) => {
+        const id = props.articleId.toString();
+        return {
+          variables: {
+            id
+          }
+        }
+      }
+  }
+)(
+  withRouter(Article)
+);
