5ce4d8a4246271dc5633bfddbb83eb4270072d30
Leah Sandacz <sandas@outlook.com>
2018-04-25 15:13:31 -0700

    Rename FindArticles query to FindArticlesByOwner

--- client-react/src/graphql/queries/FindArticlesByOwner.js
+++ client-react/src/graphql/queries/FindArticlesByOwner.js
@@ -1,7 +1,7 @@
 import gql from 'graphql-tag';
 
 export default gql`
-  query FindArticles($owner: String!) {
+  query FindArticlesByOwner($owner: String!) {
     articles(owner: $owner)
     {
       id
