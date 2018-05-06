d60c0f6ee5336bc0af2d3ffe8ea379cc92d18de7
Leah Sandacz <sandas@outlook.com>
2018-05-06 00:49:24 -0700

    Use single quotes instead of double quotes; Remove useless outer div

--- client-react/src/components/Header/Navigation/MainNav.js
+++ client-react/src/components/Header/Navigation/MainNav.js
@@ -5,25 +5,23 @@ import { Link } from 'react-router-dom';
 class MainNav extends Component {
   render() {
     return (
-      <div>
-        <Nav>
-          <NavItem>
-            <NavLink tag={Link} to="/">Home</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink tag={Link} to="/articles">Articles</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink tag={Link} to="/submit">Submit</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink tag={Link} to="/dashboard">Dashboard</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink tag={Link} to="/preferences">My Account</NavLink>
-          </NavItem>
-        </Nav>
-      </div>
+      <Nav>
+        <NavItem>
+          <NavLink tag={Link} to='/'>Home</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink tag={Link} to='/articles'>Articles</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink tag={Link} to='/submit'>Submit</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink tag={Link} to='/dashboard'>Dashboard</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink tag={Link} to='/preferences'>My Account</NavLink>
+        </NavItem>
+      </Nav>
     );
   }
 }
