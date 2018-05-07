6e5faccab8ba937b59ec640e70ed42e97bb45e85
Leah Sandacz <sandas@outlook.com>
2018-05-07 01:22:55 -0700

    Nicer spacing

--- client-react/src/graphql/queries/GetArticleFeed.js
+++ client-react/src/graphql/queries/GetArticleFeed.js
@@ -1,8 +1,21 @@
 import gql from 'graphql-tag';
 
 export default gql`
-  query GetArticleFeed($owner: String, $textSearch: String, $tags: [String], $sortBy: String, $cursor: Float, $pageLength: Float) {
-    articleFeed(owner: $owner, textSearch: $textSearch, tags: $tags, sortBy: $sortBy, cursor: $cursor, pageLength: $pageLength)
+  query GetArticleFeed(
+      $owner: String,
+      $textSearch: String,
+      $tags: [String],
+      $sortBy: String,
+      $cursor: Float,
+      $pageLength: Float)
+  {
+    articleFeed(
+      owner: $owner,
+      textSearch: $textSearch,
+      tags: $tags,
+      sortBy: $sortBy,
+      cursor: $cursor,
+      pageLength: $pageLength)
     {
       cursor
       numPages
