e84487e0e8dbfb4866a580b97081b43cc6700771
Leah Sandacz <sandas@outlook.com>
2018-05-10 04:11:59 -0700

    Navbar tabs and active tab

--- client-react/src/components/Header/Navigation/MainNav.js
+++ client-react/src/components/Header/Navigation/MainNav.js
@@ -1,29 +1,32 @@
 import React, { Component } from 'react';
 import { Nav, NavItem, NavLink } from 'reactstrap';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 
-class MainNav extends Component {
-  render() {
-    return (
-      <Nav>
-        <NavItem>
-          <NavLink tag={Link} to='/'>Home</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink tag={Link} to='/articles'>Articles</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink tag={Link} to='/submit'>Submit</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink tag={Link} to='/dashboard'>Dashboard</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink tag={Link} to='/preferences'>My Account</NavLink>
-        </NavItem>
-      </Nav>
-    );
-  }
+import MainNavLink from './MainNavLink';
+
+const MainNav = (props) => {
+  console.log('MainNav');
+  console.log(props);
+  const location = props.location.pathname;
+  return (
+    <Nav tabs>
+      <MainNavLink currentRoute={location} route='/'>
+        Home
+      </MainNavLink>
+      <MainNavLink currentRoute={location} route='/articles'>
+        Articles
+      </MainNavLink>
+      <MainNavLink currentRoute={location} route='/submit'>
+        Submit
+      </MainNavLink>
+      <MainNavLink currentRoute={location} route='/dashboard'>
+        Dashboard
+      </MainNavLink>
+      <MainNavLink currentRoute={location} route='/preferences'>
+        My Account
+      </MainNavLink>
+    </Nav>
+  );
 }
 
-export default MainNav;
+export default withRouter(MainNav);
---
+++ client-react/src/components/Header/Navigation/MainNavLink.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Nav, NavItem, NavLink } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
+
+const MainNavLink = ({currentRoute, route, children}) => {
+  let isActive;
+  if (route === '/') {
+    isActive = (currentRoute === route);
+  } else {
+    isActive = currentRoute.startsWith(route);
+  }
+  return (
+    <NavItem>
+      <NavLink
+        active={isActive}
+        tag={Link}
+        to={route}>
+        {children}
+      </NavLink>
+    </NavItem>
+  )
+}
+
+export default MainNavLink;
