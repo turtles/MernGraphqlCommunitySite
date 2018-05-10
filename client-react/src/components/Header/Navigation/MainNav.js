c0aab3b64d302fe157ab6194036d53268c1a7b6b
Leah Sandacz <sandas@outlook.com>
2018-05-10 04:14:04 -0700

    Code cleanup for MainNav

--- client-react/src/components/Header/Navigation/MainNav.js
+++ client-react/src/components/Header/Navigation/MainNav.js
@@ -1,28 +1,26 @@
 import React, { Component } from 'react';
-import { Nav, NavItem, NavLink } from 'reactstrap';
-import { Link, withRouter } from 'react-router-dom';
+import { Nav } from 'reactstrap';
+import { withRouter } from 'react-router-dom';
 
 import MainNavLink from './MainNavLink';
 
 const MainNav = (props) => {
-  console.log('MainNav');
-  console.log(props);
-  const location = props.location.pathname;
+  const { pathname } = props.location;
   return (
     <Nav tabs>
-      <MainNavLink currentRoute={location} route='/'>
+      <MainNavLink currentRoute={pathname} route='/'>
         Home
       </MainNavLink>
-      <MainNavLink currentRoute={location} route='/articles'>
+      <MainNavLink currentRoute={pathname} route='/articles'>
         Articles
       </MainNavLink>
-      <MainNavLink currentRoute={location} route='/submit'>
+      <MainNavLink currentRoute={pathname} route='/submit'>
         Submit
       </MainNavLink>
-      <MainNavLink currentRoute={location} route='/dashboard'>
+      <MainNavLink currentRoute={pathname} route='/dashboard'>
         Dashboard
       </MainNavLink>
-      <MainNavLink currentRoute={location} route='/preferences'>
+      <MainNavLink currentRoute={pathname} route='/preferences'>
         My Account
       </MainNavLink>
     </Nav>
--- client-react/src/components/Header/Navigation/MainNavLink.js
+++ client-react/src/components/Header/Navigation/MainNavLink.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Nav, NavItem, NavLink } from 'reactstrap';
+import { NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 
