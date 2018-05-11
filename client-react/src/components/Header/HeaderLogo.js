e756c98d4b5100fbdeca699a3318c1d7f97013bc
Leah Sandacz <sandas@outlook.com>
2018-05-10 18:15:31 -0700

    Styled header

--- client-react/src/components/Header/HeaderLogo.js
+++ client-react/src/components/Header/HeaderLogo.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Logo = styled.h3`
+  color: black;
+`;
 
 const HeaderLogo = ()=>(
-  <span>Leah&#39;s Site</span>
+  <Logo>Leah&#39;s Site</Logo>
 );
+
 export default HeaderLogo;
